339c419388fc5532d5974f615ab1f4ff34a110d9
hemant kumar <hemantkumar692@gmail.com>
2022-10-20 13:04:12 +0530

    slide from left, right, top,bottom

--- lib/CustomPageRoute.dart
+++ lib/CustomPageRoute.dart
@@ -4,7 +4,8 @@ import 'package:flutter/cupertino.dart';
 
 class CustomPageRoute extends PageRouteBuilder{
   final Widget childs;
-  CustomPageRoute({required this.childs}): super(
+  final AxisDirection direction;
+  CustomPageRoute({required this.childs,this.direction= AxisDirection.left}): super(
     transitionDuration: Duration(seconds: 1),
     reverseTransitionDuration: Duration(seconds: 1),
     pageBuilder:(context,animation,secondaryAnimation)=> childs
@@ -16,10 +17,23 @@ class CustomPageRoute extends PageRouteBuilder{
   Widget buildTransitions(BuildContext context,Animation<double> animation ,Animation<double> secondaryAnimation, Widget child ){
    return SlideTransition(
      position: Tween<Offset>(
-       begin: Offset(-1,0),
+       begin: beginOffset(),
        end: Offset.zero,
      ).animate(animation),
       child: child,
     );
   }
+
+ Offset beginOffset() {
+    switch (direction){
+      case AxisDirection.up:
+        return Offset(0, 1);
+      case AxisDirection.down:
+        return Offset(0 , -1);
+      case AxisDirection.right:
+        return Offset(-1, 0);
+      case AxisDirection.left:
+        return Offset(1, 0);
+    }
+ }
 }
